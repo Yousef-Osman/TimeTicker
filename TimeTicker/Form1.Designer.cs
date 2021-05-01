daae71a6189b2bf718d2ce9bb9c901ea1d4df221
Yousef Osman <yousef.osman.31@gmail.com>
2021-05-01 13:23:52 +0200

    Add project files.

---
+++ TimeTicker/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace TimeTicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opacityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.TimerOkBtn = new System.Windows.Forms.Button();
+            this.DTBEnd = new System.Windows.Forms.DateTimePicker();
+            this.DTBStart = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem,
+            this.opacityToolStripMenuItem,
+            this.timerToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(130, 76);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // opacityToolStripMenuItem
+            // 
+            this.opacityToolStripMenuItem.Name = "opacityToolStripMenuItem";
+            this.opacityToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.opacityToolStripMenuItem.Text = "Opacity";
+            this.opacityToolStripMenuItem.Click += new System.EventHandler(this.opacityToolStripMenuItem_Click);
+            // 
+            // timerToolStripMenuItem
+            // 
+            this.timerToolStripMenuItem.Name = "timerToolStripMenuItem";
+            this.timerToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.timerToolStripMenuItem.Text = "Timer";
+            this.timerToolStripMenuItem.Click += new System.EventHandler(this.timerToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.circularProgressBar1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(192, 244);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Aqua;
+            this.label1.Location = new System.Drawing.Point(41, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Remainig";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar1.AnimationSpeed = 500;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.Aqua;
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.Black;
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(19, 67);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.DarkSlateGray;
+            this.circularProgressBar1.OuterMargin = -8;
+            this.circularProgressBar1.OuterWidth = 8;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.Aqua;
+            this.circularProgressBar1.ProgressWidth = 7;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.circularProgressBar1.Size = new System.Drawing.Size(150, 150);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.Black;
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.Black;
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 2;
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(0);
+            this.circularProgressBar1.Value = 68;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.OkBtn);
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(192, 244);
+            this.panel2.TabIndex = 4;
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.BackColor = System.Drawing.Color.Black;
+            this.OkBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.OkBtn.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.OkBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
+            this.OkBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.OkBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OkBtn.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OkBtn.ForeColor = System.Drawing.Color.Aqua;
+            this.OkBtn.Location = new System.Drawing.Point(56, 173);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(75, 35);
+            this.OkBtn.TabIndex = 5;
+            this.OkBtn.Text = "Ok";
+            this.OkBtn.UseVisualStyleBackColor = false;
+            this.OkBtn.Click += new System.EventHandler(this.OkBtn_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(21, 97);
+            this.trackBar1.Minimum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(151, 56);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 8;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Aqua;
+            this.label2.Location = new System.Drawing.Point(51, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Opacity";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.TimerOkBtn);
+            this.panel3.Controls.Add(this.DTBEnd);
+            this.panel3.Controls.Add(this.DTBStart);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(192, 244);
+            this.panel3.TabIndex = 4;
+            // 
+            // TimerOkBtn
+            // 
+            this.TimerOkBtn.BackColor = System.Drawing.Color.Black;
+            this.TimerOkBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.TimerOkBtn.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.TimerOkBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
+            this.TimerOkBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Teal;
+            this.TimerOkBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TimerOkBtn.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimerOkBtn.ForeColor = System.Drawing.Color.Aqua;
+            this.TimerOkBtn.Location = new System.Drawing.Point(59, 188);
+            this.TimerOkBtn.Name = "TimerOkBtn";
+            this.TimerOkBtn.Size = new System.Drawing.Size(75, 35);
+            this.TimerOkBtn.TabIndex = 6;
+            this.TimerOkBtn.Text = "Ok";
+            this.TimerOkBtn.UseVisualStyleBackColor = false;
+            this.TimerOkBtn.Click += new System.EventHandler(this.TimerOkBtn_Click);
+            // 
+            // DTBEnd
+            // 
+            this.DTBEnd.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.DTBEnd.Font = new System.Drawing.Font("Tahoma", 7.5F);
+            this.DTBEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DTBEnd.Location = new System.Drawing.Point(21, 147);
+            this.DTBEnd.Margin = new System.Windows.Forms.Padding(0);
+            this.DTBEnd.Name = "DTBEnd";
+            this.DTBEnd.Size = new System.Drawing.Size(151, 23);
+            this.DTBEnd.TabIndex = 4;
+            // 
+            // DTBStart
+            // 
+            this.DTBStart.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.DTBStart.Font = new System.Drawing.Font("Tahoma", 7.5F);
+            this.DTBStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DTBStart.Location = new System.Drawing.Point(21, 89);
+            this.DTBStart.Margin = new System.Windows.Forms.Padding(0);
+            this.DTBStart.Name = "DTBStart";
+            this.DTBStart.Size = new System.Drawing.Size(151, 23);
+            this.DTBStart.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Aqua;
+            this.label5.Location = new System.Drawing.Point(76, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 21);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "End";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Aqua;
+            this.label4.Location = new System.Drawing.Point(72, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Start";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Aqua;
+            this.label3.Location = new System.Drawing.Point(37, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Set Timer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(192, 244);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ToolStripMenuItem opacityToolStripMenuItem;
+        private System.Windows.Forms.Button OkBtn;
+        private System.Windows.Forms.ToolStripMenuItem timerToolStripMenuItem;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DateTimePicker DTBStart;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button TimerOkBtn;
+        private System.Windows.Forms.DateTimePicker DTBEnd;
+    }
+}
+
---
+++ TimeTicker/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace TimeTicker
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer1;
+        private bool mouseDown;
+        private Point lastLocation;
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 15, 15));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1_Tick(sender, e);
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000 * 600;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var start = new DateTime(2021, 4, 28, 15, 0, 0);
+            var end = new DateTime(2021, 5, 3, 22, 0, 0);
+
+            updateProgressBar(start, end);
+        }
+
+        private void updateProgressBar(DateTime start, DateTime end)
+        {
+            var now = DateTime.Now;
+            var remainig = Convert.ToDouble((end - now).Ticks);
+            var full = Convert.ToDouble((end - start).Ticks);
+
+            var progress = remainig / full;
+
+            circularProgressBar1.Minimum = 0;
+            circularProgressBar1.Maximum = 100;
+
+            circularProgressBar1.Value = (int)(Math.Round(progress, 2) * 100);
+            circularProgressBar1.Text = $"{(Math.Round(progress, 4) * 100).ToString()} %";
+            circularProgressBar1.Update();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            this.Opacity = trackBar1.Value * 0.1;
+        }
+
+        private void opacityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = (int)(this.Opacity * 10);
+            panel1.Visible = false;
+            panel3.Visible = false;
+            panel2.Visible = true;
+        }
+
+        private void OkBtn_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            panel3.Visible = false;
+            panel1.Visible = true;
+        }
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Clicks == 1)
+                mouseDown = true;
+
+            lastLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void timerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = true;
+        }
+
+        private void TimerOkBtn_Click(object sender, EventArgs e)
+        {
+            var start = DTBStart.Value;
+            var end = DTBEnd.Value;
+
+            updateProgressBar(start, end);
+
+            panel3.Visible = false;
+            panel2.Visible = false;
+            panel1.Visible = true;
+        }
+    }
+}
